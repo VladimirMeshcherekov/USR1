d12d5ef8213b6ba6856f873f2db03cb69539489e
user <3gotosnapp@gmail.com>
2020-10-06 21:09:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите массу: ");
+            double m = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Введите радиус: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Введите высота: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+
+
+            double g = 0.0000000000667408; //да, так делать нельзя, но у меня лапки и я не могу по другому
+
+
+            Console.WriteLine(Math.Sqrt((g * m) / (r + h)));
+
+        }
+    }
+}
